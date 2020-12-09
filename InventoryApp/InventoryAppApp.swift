c6a2746b8766b5a0f88bc5aa817d83acd7426ae7
Ryan Smetana <ryan.smetana@xavware.com>
2020-12-09 01:11:02 -0500

    Initial Commit

---
+++ InventoryApp/InventoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryAppApp.swift
+//  InventoryApp
+//
+//  Created by Ryan Smetana on 12/9/20.
+//
+
+import SwiftUI
+
+@main
+struct InventoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
