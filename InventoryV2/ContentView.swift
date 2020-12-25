b4327f4b4c370fd86163cee001fa32058810e39c
Ryan Smetana <ryan.smetana@xavware.com>
2020-12-25 00:30:08 -0500

    Initial Commit

---
+++ InventoryV2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InventoryV2
+//
+//  Created by Ryan Smetana on 12/25/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InventoryV2/InventoryV2App.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryV2App.swift
+//  InventoryV2
+//
+//  Created by Ryan Smetana on 12/25/20.
+//
+
+import SwiftUI
+
+@main
+struct InventoryV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
