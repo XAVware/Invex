56b0bc2f1cd778309372b4255c6d9ade47547a35
Ryan Smetana <ryan.smetana@xavware.com>
2021-01-20 14:29:16 -0500

    Initial Commit

---
+++ ConcessionTracker/ConcessionTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcessionTrackerApp.swift
+//  ConcessionTracker
+//
+//  Created by Ryan Smetana on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct ConcessionTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
