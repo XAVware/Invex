c4ad59fbe785b22a07028ad1799ea8c5e980e7f5
Ryan Smetana <ryan.smetana@xavware.com>
2021-10-14 19:24:55 -0400

    Initial Commit

---
+++ InventoryTracker_10.2021/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InventoryTracker_10.2021
+//
+//  Created by Ryan Smetana on 10/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InventoryTracker_10.2021/InventoryTracker_10_2021App.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryTracker_10_2021App.swift
+//  InventoryTracker_10.2021
+//
+//  Created by Ryan Smetana on 10/14/21.
+//
+
+import SwiftUI
+
+@main
+struct InventoryTracker_10_2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
