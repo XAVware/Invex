cc0905094a29d9c45f8c8a1e655f585d4845d3f1
Ryan Smetana <ryan.smetana@xavware.com>
2023-05-29 04:19:26 -0400

    Filter sales by selected date range.

--- InventoryX/Views/SalesHistoryView.swift
+++ InventoryX/Views/SalesHistoryView.swift
@@ -4,9 +4,8 @@ import SwiftUI
 import RealmSwift
 
 struct SalesHistoryView: View {
-    let saleDateManager: SaleDateManager = SaleDateManager()
+//    let saleDateManager: SaleDateManager = SaleDateManager()
     var columnTitles: [String] = ["Timestamp", "No. Items", "Subtotal"]
-//    var filters: [String] = ["Today", "Yesterday", "This Week", "Last Week"]
     
     enum DateRanges: String, CaseIterable, Identifiable {
         var id: UUID {
@@ -18,69 +17,116 @@ struct SalesHistoryView: View {
         case thisWeek = "This Week"
         case lastWeek = "Last Week"
         
-        var startDate: Date {
-            return Date().startOfDay()
+        var calendar: Calendar {
+            var calendar = Calendar.current
+            calendar.timeZone = TimeZone(secondsFromGMT: 0)!
+            calendar.firstWeekday = 1
+            return calendar
+        }
+
+        var startOfToday: Date {
+            return calendar.startOfDay(for: Date())
         }
         
-        var endDate: Date {
-            let start: Date = startDate
-            let startOfNextDay = Calendar.current.date(byAdding: .day, value: 1, to: start)
-            let endOfDay = Calendar.current.date(byAdding: .second, value: -1, to: startOfNextDay ?? Date())
-            return endOfDay ?? Date()
+        var endOfToday: Date {
+            return calendar.date(bySettingHour: 23, minute: 59, second: 59, of: Date())!
         }
+        
+        var yesterday: Date {
+            return calendar.date(byAdding: .day, value: -1 , to: Date())!
+        }
+        
+        var startOfYesterday: Date {
+            return calendar.startOfDay(for: yesterday)
+        }
+        
+        var endOfYesterday: Date {
+            return self.calendar.date(bySettingHour: 23, minute: 59, second: 59, of: self.yesterday)!
+        }
+        
+        var thisWeek: [Date] {
+            var week: [Date] = []
+            if let weekInterval = self.calendar.dateInterval(of: .weekOfYear, for: Date()) {
+                for i in 0...6 {
+                    if let day = self.calendar.date(byAdding: .day, value: i, to: weekInterval.start) {
+                        week.append(day)
+                    }
+                }
+            }
+            return week
+        }
+        
+        var startOfThisWeek: Date {
+            return self.thisWeek[0]
+        }
+        
+        var endOfThisWeek: Date {
+            let lastDay = self.thisWeek[self.thisWeek.count - 1]
+            return self.calendar.date(bySettingHour: 23, minute: 59, second: 59, of: lastDay)!
+        }
+        
+        var lastWeek: [Date] {
+            let oneWeekAgo: Date = calendar.date(byAdding: .weekOfYear, value: -1, to: Date())!
+            var week: [Date] = []
+            if let weekInterval = self.calendar.dateInterval(of: .weekOfYear, for: oneWeekAgo) {
+                for i in 0...6 {
+                    if let day = self.calendar.date(byAdding: .day, value: i, to: weekInterval.start) {
+                        week.append(day)
+                    }
+                }
+            }
+            return week
+        }
+        
+        var startOfLastWeek: Date {
+            return self.lastWeek[0]
+        }
+        
+        var endOfLastWeek: Date {
+            let lastDay = self.lastWeek[self.lastWeek.count - 1]
+            return self.calendar.date(bySettingHour: 23, minute: 59, second: 59, of: lastDay)!
+        }
+    
         
         var realmPredicateForRange: NSPredicate {
-            return NSPredicate(format: "timestamp BETWEEN {%@, %@}", startDate as NSDate, endDate as NSDate)
+            switch self {
+            case .today:
+                return NSPredicate(format: "timestamp BETWEEN {%@, %@}", startOfToday as NSDate, endOfToday as NSDate)
+            case .yesterday:
+                return NSPredicate(format: "timestamp BETWEEN {%@, %@}", startOfYesterday as NSDate, endOfYesterday as NSDate)
+            case .lastWeek:
+                return NSPredicate(format: "timestamp BETWEEN {%@, %@}", startOfLastWeek as NSDate, endOfLastWeek as NSDate)
+            case .thisWeek:
+                return NSPredicate(format: "timestamp BETWEEN {%@, %@}", startOfThisWeek as NSDate, endOfThisWeek as NSDate)
+            }
         }
     }
     
     @State var selectedDateRange: DateRanges = DateRanges.today
     @State var isShowingDetail: Bool = false
     
-//    @ObservedResults(SaleEntity.self) var salesInRange
-    @State var salesInRange: [SaleEntity] = []
-    //For Testing
-//    var salesInRange: [SaleEntity] = [SaleEntity.todaySale1, SaleEntity.yesterdaySale1]
-    
-//    var rangeSales: Results<SaleEntity> {
-//        switch selectedFilter {
-//        case "Today":
-//            return try! Realm().objects(SaleEntity.self).filter(NSPredicate(format: "timestamp BETWEEN {%@, %@}", self.saleDateManager.startOfToday as NSDate, self.saleDateManager.endOfToday as NSDate))
-//        case "Yesterday":
-//            return try! Realm().objects(SaleEntity.self).filter(NSPredicate(format: "timestamp BETWEEN {%@, %@}", self.saleDateManager.startOfYesterday as NSDate, self.saleDateManager.endOfYesterday as NSDate))
-//        case "This Week":
-//            return try! Realm().objects(SaleEntity.self).filter(NSPredicate(format: "timestamp BETWEEN {%@, %@}", self.saleDateManager.startOfThisWeek as NSDate, self.saleDateManager.endOfThisWeek as NSDate))
-//        case "Last Week":
-//            return try! Realm().objects(SaleEntity.self).filter(NSPredicate(format: "timestamp BETWEEN {%@, %@}", self.saleDateManager.startOfLastWeek as NSDate, self.saleDateManager.endOfLastWeek as NSDate))
-//        default:
-//            return try! Realm().objects(SaleEntity.self)
-//        }
-//    }
+    @ObservedResults(SaleEntity.self) var allSales
+
+    @State var rangeSales: [SaleEntity] = []
     
     var rangeTotal: Double {
         var tempTotal: Double = 0
-        for sale in salesInRange {
-            tempTotal += sale.total
-        }
+//        if let rangeSales = rangeSales {
+            for sale in rangeSales {
+                tempTotal += sale.total
+            }
+//        }
         return tempTotal
     }
     
     func updateSales(newRange: DateRanges) {
-        print("Range Changed To : \(newRange), new values:")
-        print("Start - \(newRange.startDate)")
-        print("End - \(newRange.endDate)")
-        print("Predicate - \(newRange.realmPredicateForRange)")
-        do {
-            let realm = try Realm()
-            let rangeSales = realm.objects(SaleEntity.self).filter(newRange.realmPredicateForRange)
-            for sale in rangeSales {
-                let tempSale = SaleEntity(timestamp: sale.timestamp, total: sale.total)
-                salesInRange.append(tempSale)
-            }
-            print("Finished: \(salesInRange)")
-        } catch {
-            print(error.localizedDescription)
-        }
+        var sales: [SaleEntity] = []
+        let salesInRange = allSales.filter(selectedDateRange.realmPredicateForRange)
+        salesInRange.forEach({ sale in
+            let tempSale = SaleEntity(timestamp: sale.timestamp, total: sale.total)
+            sales.append(tempSale)
+        })
+        rangeSales = sales
     }
     
     var body: some View {
@@ -117,16 +163,20 @@ struct SalesHistoryView: View {
                                 
                 List {
                     Section {
-                        ForEach(salesInRange, id: \.self) { sale in
-                            HStack {
-                                Text("\(sale.timestamp.formatted(date: .numeric, time: .shortened))")
-                                Spacer()
-                                Text(sale.total.formatToCurrencyString())
-                            } //: HStack
-                            .onTapGesture {
-                                isShowingDetail.toggle()
+//                        if let rangeSales = salesInRange {
+                            ForEach(rangeSales, id: \.self) { sale in
+                                HStack {
+                                    Text("\(sale.timestamp.formatted(date: .numeric, time: .shortened))")
+                                    Spacer()
+                                    Text(sale.total.formatToCurrencyString())
+                                } //: HStack
+                                .onTapGesture {
+                                    isShowingDetail.toggle()
+                                }
                             }
-                        }
+//                        } else {
+//                            Text("No Sales")
+//                        }
                     } header: {
                         Text("Sales")
                             .modifier(TextMod(.largeTitle, .semibold, .black))
@@ -145,6 +195,7 @@ struct SalesHistoryView: View {
             .background(secondaryBackground)
             .onAppear {
                 updateSales(newRange: selectedDateRange)
+                print(allSales)
             }
             .sheet(isPresented: $isShowingDetail) {
                 SaleDetailView()
